b4751b30e7932509768e47169485ac31440899ac
hchen19 <chenhaoyuan2018@gmail.com>
2025-02-02 20:10:23 -0600

    remove unnecessary import

--- dak/utils/sparse_design/__init__.py
+++ dak/utils/sparse_design/__init__.py
@@ -1,8 +1,6 @@
-from .clenshaw_curtis import cc_design
 from .design_class import HyperbolicCrossDesign, SparseGridDesign
 
 __all__ = [
-    "cc_design",
     "HyperbolicCrossDesign",
     "SparseGridDesign",
 ]
